5f52a274eed359d2080f5a7da89130f8c5ff15de
Darcy Rogers <darcy.rogers@oit.edu>
2026-02-19 16:20:14 -0800

    Created and finished the dataset_creation function

--- Neural-Network.py
+++ Neural-Network.py
@@ -8,41 +8,46 @@ import kagglehub
 
 #TODO
 #Change seed to actually be random for final version set seed is fine for small scale testing
-#Move dataset creation and management to independant function
 #Make neural network to process dataset confer with group for details
 #Remove commented code for final version
 
+#Function creates the training and validation datasets and returns aforementioned datasets
+def dataset_creation():
+  # Download latest version also concatenate \\rocks to the end to end up in the correct directory
+  path = kagglehub.dataset_download("neelgajare/rocks-dataset")
+  path+= "\\rocks"
+  #print("Path to dataset files:", path) #commented out for brevity maintained for so we can check
+  
+  #code nicked from google gemini because the relavant kaggle tutorial is bad
+  batch_size = 32
+  img_height = 512   #Sizes are overkilled because I am lazy and its easier then dynamically loading images by alot
+  img_width = 512
+  
+  #training Dataset reserves 20% of dataset for validation
+  train_ds = tf.keras.utils.image_dataset_from_directory(
+    path,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+  )
+  
+  #validation dataset uses 20% of dataset to make sure we train right
+  val_ds = tf.keras.utils.image_dataset_from_directory(
+    path,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+  )
 
-# Download latest version also concatenate \\rocks to the end to end up in the correct directory
-path = kagglehub.dataset_download("neelgajare/rocks-dataset")
-path+= "\\rocks"
-#print("Path to dataset files:", path) #commented out for brevity maintained for so we can check
+  #returns training and validation sets
+  return train_ds, val_ds
 
-#code nicked from google gemini because the relavant kaggle tutorial is bad
-batch_size = 32
-img_height = 512   #Sizes are overkilled because I am lazy and its easier then dynamically loading images by alot
-img_width = 512
+dataset_creation()
 
-#training Dataset reserves 20% of dataset for validation
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  path,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size
-)
-
-#validation dataset uses 20% of dataset to make sure we train right
-val_ds = tf.keras.utils.image_dataset_from_directory(
-  path,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size
-)
-
-#if you want to verify the classes are correctly generating
+#if you want to verify the classes are correctly generating uncomment following 2 lines
 #class_names = train_ds.class_names
 #print("Found these classes:", class_names)
